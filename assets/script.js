6e312438a46e3f69fedf036b8ac6ad464e07fcba
Adrianna Derkacz <a.derkacz@gmail.com>
2024-05-22 20:54:22 +0200

    Added functionality do the icon - it switches accordingly to the mode user selects

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.querySelector(".slider.round")
+const iconEl = document.getElementById("icon-el")
 
 toggleBtn.addEventListener("click", function() {
     let element = document.body;
     element.classList.toggle("dark-mode");
-})
+
+    if (element.classList.contains("dark-mode")) {
+        iconEl.className = 'fa-solid fa-moon';
+        iconEl.style.color = '#ccc1ff';
+    } else {
+        iconEl.className = 'fa-solid fa-sun-bright';
+        iconEl.style.color = '#ccc1ff';
+    }
+});
